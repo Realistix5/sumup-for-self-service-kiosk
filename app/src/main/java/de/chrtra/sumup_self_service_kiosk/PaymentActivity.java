4591ba5a57f7a2c23d7d3ff24cda312a65943189
Christopher Trautmann <christopher.b.trautmann@stud.h-da.de>
2024-06-08 16:57:05 +0200

    made most sumup payment fields editable per payment through query parameters

--- app/src/main/java/de/chrtra/sumup_self_service_kiosk/PaymentActivity.java
+++ app/src/main/java/de/chrtra/sumup_self_service_kiosk/PaymentActivity.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class PaymentActivity extends Activity {
 
@@ -33,34 +34,91 @@ public class PaymentActivity extends Activity {
             }
 
             if (queryParams.containsKey("total")) {
-                String amount = queryParams.get("total");
-                String title = sharedPreferences.getString("payment_title", "");
+                // Initialize payment builder
+                SumUpPayment.Builder paymentBuilder = SumUpPayment.builder();
 
-                SumUpPayment.Builder paymentBuilder = SumUpPayment.builder()
-                        .total(new BigDecimal(amount)) // Der minimale Betrag ist 1.00
-                        .currency(SumUpPayment.Currency.EUR)
-                        .title(title);
+                // Add total
+                paymentBuilder.total(new BigDecimal(queryParams.get("total")));
 
-                SumUpPayment.Currency currency;
-                try {
-                    currency = SumUpPayment.Currency.valueOf(sharedPreferences.getString("currency", "EUR"));
-                } catch (IllegalArgumentException e) {
-                    currency = SumUpPayment.Currency.valueOf( "EUR");
-                }
-                paymentBuilder.currency(currency);
-
-                if (sharedPreferences.getBoolean("tip_on_card_reader", false)) {
-                    paymentBuilder.tipOnCardReader();
-                }
-                if (sharedPreferences.getBoolean("skip_failed_screen", false)) {
-                    paymentBuilder.skipFailedScreen();
-                }
-                if (sharedPreferences.getBoolean("skip_success_screen", false)) {
-                    paymentBuilder.skipSuccessScreen();
+                // Add currency
+                if (queryParams.containsKey("currency")) {
+                    paymentBuilder.currency(SumUpPayment.Currency.valueOf(queryParams.get("currency")));
+                } else {
+                    try {
+                        paymentBuilder.currency(SumUpPayment.Currency.valueOf(sharedPreferences.getString("currency", "EUR")));
+                    } catch (IllegalArgumentException e) {
+                        paymentBuilder.currency(SumUpPayment.Currency.EUR);
+                    }
                 }
 
+                // Add title
+                if (queryParams.containsKey("title")) {
+                    paymentBuilder.title(queryParams.get("title"));
+                } else {
+                    paymentBuilder.title(sharedPreferences.getString("payment_title", ""));
+                }
+
+                // Add tip
+                if (queryParams.containsKey("tip")) {
+                    paymentBuilder.tip(new BigDecimal(queryParams.get("tip")));
+                }
+
+                // Add receiptEmail
+                if (queryParams.containsKey("receipt_email")) {
+                    paymentBuilder.receiptEmail(queryParams.get("receipt_email"));
+                }
+
+                // Add receiptSMS
+                if (queryParams.containsKey("receipt_sms")) {
+                    paymentBuilder.receiptEmail(queryParams.get("receipt_sms"));
+                }
+
+                // Add foreignTransactionId
+                if (queryParams.containsKey("foreign_transaction_id")) {
+                    paymentBuilder.foreignTransactionId(Objects.requireNonNull(queryParams.get("foreign_transaction_id")));
+                }
+
+                // Add tip
+                if (queryParams.containsKey("tip")) {
+                    paymentBuilder.tip(new BigDecimal(queryParams.get("tip")));
+                }
+
+                // Add booleans
+                // Tip on card reader
+                if (queryParams.containsKey("tip_on_card_reader")) {
+                    if (queryParams.get("tip_on_card_reader").equals("1")){
+                        paymentBuilder.tipOnCardReader();
+                    }
+                } else {
+                    if (sharedPreferences.getBoolean("tip_on_card_reader", false)) {
+                        paymentBuilder.tipOnCardReader();
+                    }
+                }
+                // Skip failed screen
+                if (queryParams.containsKey("skip_failed_screen")) {
+                    if (queryParams.get("skip_failed_screen").equals("1")){
+                        paymentBuilder.skipFailedScreen();
+                    }
+                } else {
+                    if (sharedPreferences.getBoolean("skip_failed_screen", false)) {
+                        paymentBuilder.skipFailedScreen();
+                    }
+                }
+                // Skip success screen
+                if (queryParams.containsKey("skip_success_screen")) {
+                    if (queryParams.get("skip_success_screen").equals("1")){
+                        paymentBuilder.skipSuccessScreen();
+                    }
+                } else {
+                    if (sharedPreferences.getBoolean("skip_success_screen", false)) {
+                        paymentBuilder.skipSuccessScreen();
+                    }
+                }
+
+                // Build payment
                 SumUpPayment payment = paymentBuilder.build();
 
+                // Start checkout
                 SumUpAPI.checkout(this, payment, 2); // '2' ist der Request-Code für die Activity-Result-Rückgabe
             }
         }
